e396dcf3f80dd041608bfe8f040036f1983705fa
Berkay Babaloglu <bkybabal@gmail.com>
2026-06-10 22:48:01 +0000

    go

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,9 @@
 import { Timeline } from "./TimelineChart/Timeline";
 import { sampleChartProps } from './TimelineChart/sampleProps'
-import { type ApiResponse } from "./types";
-
-const url = 'api.alyi.uk/sessions';
-const res  = await fetch(url);
-const json: ApiResponse = res.json();
 
 
 export default function App() {
   
- 
-  json.sessions
 
 
 
